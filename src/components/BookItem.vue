4edc0ff042a305efbac64a5de881341c85726a3d
Javi <javi.cb@gmail.com>
2021-01-13 10:39:34 +0100

    creating child component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li>{{ book.title }}, {{ book.author }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "BookItem",
+      props: ["book"],
+    };
+  },
+};
+</script>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
